cb23a00d8b0005b8f042194e92dd5784eb5d9bb4
Jarred Capellman <jcapellman@hotmail.com>
2018-01-06 13:35:01 -0600

    Added: Initial commit of NearestNeighborScaler

---
+++ src/texelfx.library/Scalers/NearestNeighborScaler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+
+using texelfx.library.Enums;
+
+namespace texelfx.library.Scalers
+{
+    public class NearestNeighborScaler : BaseScaler
+    {
+        public override StatusCodes Scale(int width, int height, string inputFileName, string outputFileName)
+        {
+            if (!File.Exists(inputFileName))
+            {
+                return StatusCodes.INPUT_FILE_DOES_NOT_EXIST;
+            }
+
+            try
+            {
+                using (var bitmapStream = File.Open(inputFileName, FileMode.Open))
+                {
+                    var img = Image.FromStream(bitmapStream);
+
+                    using (var graphics = Graphics.FromImage(img))
+                    {
+                        graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                        graphics.DrawImage(img, 0, 0, width, height);
+
+                        var mBitmap = new Bitmap(width, height, graphics);
+                        mBitmap.Save(GetOutputFileName(inputFileName, outputFileName), ImageFormat.Png);
+                    }
+
+                    return StatusCodes.SUCCESSFULLY_SCALED;
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCodes.INTERNAL_ERROR;
+            }
+        }
+    }
+}
