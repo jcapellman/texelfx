f31aabcd7e621cd7e5e09522a243420f02ec9c89
Jarred Capellman <jcapellman@hotmail.com>
2018-01-06 13:33:59 -0600

    Added: BaseScaler for implementing Image Scalers

---
+++ src/texelfx.library/BaseScaler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using texelfx.library.Enums;
+
+namespace texelfx.library
+{
+    public abstract class BaseScaler
+    {
+        protected static string GetOutputFileName(string inputFileName, string outputFileName) => !string.IsNullOrEmpty(outputFileName) ? outputFileName : $"{DateTime.Now.Ticks}_{inputFileName}";
+
+        public abstract StatusCodes Scale(int width, int height, string inputFileName, string outputFileName);        
+    }
+}
