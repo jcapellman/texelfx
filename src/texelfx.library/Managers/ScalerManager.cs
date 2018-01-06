57f73caa7417d3c926d5b5bb7c1796df7eba209f
Jarred Capellman <jcapellman@hotmail.com>
2018-01-06 13:34:37 -0600

    Added: ScalerManager to handle gathering all of the Scalers for the REST Service and MVC App

---
+++ src/texelfx.library/Managers/ScalerManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace texelfx.library.Managers
+{
+    public class ScalerManager
+    {
+        public static IEnumerable<BaseScaler> GetScalers() => Assembly.GetAssembly(typeof(ScalerManager)).GetTypes()
+            .Where(a => !a.IsAbstract && a.BaseType == typeof(BaseScaler))
+            .Select(b => (BaseScaler) Activator.CreateInstance(b));
+    }
+}
