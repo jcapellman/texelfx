6ae8da951e7305d1efb276424708d4ce67d2d8ab
Jarred Capellman <jcapellman@hotmail.com>
2018-01-06 20:16:01 -0600

    Added: Initial commit of the FileUploadModel

---
+++ src/texelfx.mvc/Models/FileUploadModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using Microsoft.AspNetCore.Http;
+
+namespace texelfx.mvc.Models
+{
+    public class FileUploadModel
+    {
+        public List<string> Scalers { get; set; }
+
+        [Required]
+        public IFormFile UploadFile { get; set; }
+
+        [Required]
+        public string ScalerType { get; set; }
+    }
+}
