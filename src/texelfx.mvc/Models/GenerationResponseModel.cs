057994ec2b134a5a7a45541aa9a931ed5c9704df
Jarred Capellman <jcapellman@hotmail.com>
2018-01-06 20:16:22 -0600

    Added: Initial commit of the GenerationResponseModel

---
+++ src/texelfx.mvc/Models/GenerationResponseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace texelfx.mvc.Models
+{
+    public class GenerationResponseModel
+    {
+        public string ScalerType { get; set; }
+
+        public byte[] OriginalBytes { get; set; }
+
+        public byte[] ResizedBytes { get; set; }
+    }
+}
