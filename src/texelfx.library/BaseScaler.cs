e4117c015b81e353e1aa90b5fb34187bf86f4fe3
Jarred Capellman <jcapellman@hotmail.com>
2018-01-06 20:13:57 -0600

    Change: Updated the BaseScaler to incorporate the new file-less approach

--- src/texelfx.library/BaseScaler.cs
+++ src/texelfx.library/BaseScaler.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
-using texelfx.library.Enums;
-
 namespace texelfx.library
 {
     public abstract class BaseScaler
     {
+        public abstract string Name { get; }
+
         protected static string GetOutputFileName(string inputFileName, string outputFileName) => !string.IsNullOrEmpty(outputFileName) ? outputFileName : $"{DateTime.Now.Ticks}_{inputFileName}";
 
-        public abstract StatusCodes Scale(int width, int height, string inputFileName, string outputFileName);        
+        public abstract byte[] Scale(int width, int height, byte[] originalBytes);        
     }
 }
