66dfb1dbd946779cd0681999c4771fb9c77c72d6
Jarred Capellman <jcapellman@hotmail.com>
2018-01-10 20:50:24 -0600

    Added: Initial work on the JCScaler class

---
+++ src/texelfx.library/Scalers/JCScaler.cs
@@ -0,0 +1,17 @@
+﻿using texelfx.library.Objects;
+
+namespace texelfx.library.Scalers
+{
+    public class JCScaler : BaseScaler
+    {
+        public override string Name => "JC Scaler";
+
+        public override ScalerResponseItem Scale(int scaleMultiplier, byte[] originalBytes)
+        {
+            return new ScalerResponseItem
+            {
+                ScaledBytes = originalBytes
+            };
+        }
+    }
+}
