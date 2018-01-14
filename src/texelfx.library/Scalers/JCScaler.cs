7c867eaea98ca07c48a65a7daebd7c2bbd636fb9
Jarred Capellman <jcapellman@hotmail.com>
2018-01-14 13:36:48 -0600

    Added: Initial work on integrating TensorFlow into the JCScaler class

--- src/texelfx.library/Scalers/JCScaler.cs
+++ src/texelfx.library/Scalers/JCScaler.cs
@@ -1,4 +1,7 @@
-﻿using texelfx.library.Objects;
+﻿using texelfx.library.Common;
+using texelfx.library.Objects;
+
+using TensorFlowSharpCore;
 
 namespace texelfx.library.Scalers
 {
@@ -8,6 +11,23 @@ namespace texelfx.library.Scalers
 
         public override ScalerResponseItem Scale(int scaleMultiplier, byte[] originalBytes)
         {
+            var graph = new TFGraph();
+
+            // TODO Load Model
+
+            using (var session = new TFSession(graph))
+            {
+                var tensor = ImageUtil.CreateTensorFromImageBytes(originalBytes);
+
+                var runner = session.GetRunner();
+
+                // TODO AddInput to the runner with the data from above
+
+                var output = runner.Run();
+                
+                // TODO Parse output
+            }
+
             return new ScalerResponseItem
             {
                 ScaledBytes = originalBytes
