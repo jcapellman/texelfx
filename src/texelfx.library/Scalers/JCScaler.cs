935e93aee48c401fb0395585b459accf45d492af
Jarred Capellman <jcapellman@hotmail.com>
2018-01-14 14:38:40 -0600

    Change: Updated the JCScaler to match the new ImageUtil method parameters

--- src/texelfx.library/Scalers/JCScaler.cs
+++ src/texelfx.library/Scalers/JCScaler.cs
@@ -1,4 +1,6 @@
-﻿using texelfx.library.Common;
+﻿using System.Numerics;
+
+using texelfx.library.Common;
 using texelfx.library.Objects;
 
 using TensorFlowSharpCore;
@@ -7,6 +9,8 @@ namespace texelfx.library.Scalers
 {
     public class JCScaler : BaseScaler
     {
+        private static readonly Vector2 MODEL_IMAGE_SIZE = new Vector2(1920, 1080);
+
         public override string Name => "JC Scaler";
 
         public override ScalerResponseItem Scale(int scaleMultiplier, byte[] originalBytes)
@@ -17,7 +21,7 @@ namespace texelfx.library.Scalers
 
             using (var session = new TFSession(graph))
             {
-                var tensor = ImageUtil.CreateTensorFromImageBytes(originalBytes);
+                var tensor = ImageUtil.CreateTensorFromImageBytes(originalBytes, MODEL_IMAGE_SIZE);
 
                 var runner = session.GetRunner();
 
