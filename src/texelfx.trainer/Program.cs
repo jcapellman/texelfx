c3e35376d717d540781863c738e93c4f289a8384
Jarred Capellman <jcapellman@hotmail.com>
2018-06-30 18:21:20 -0500

    Change: Updated all of the projects to .NET Core 2.1

---
+++ src/texelfx.trainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace texelfx.trainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
