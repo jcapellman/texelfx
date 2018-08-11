d9c52782d6928371c90246bf0335b8f67d6234ac
Jarred Capellman <jcapellman@hotmail.com>
2018-08-11 09:46:44 -0500

    Added: Initial work on the service and platform libraries

---
+++ src/texelfx.service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace texelfx.service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
